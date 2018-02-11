43f399cd2e01c545fece846518ddfdb02c9184a7
Mike Azarov <draftpodium@gmail.com>
2018-02-11 15:15:19 +0700

    multi-threading TCP echo server for STEPIC.ORG course

--- servak.py
+++ servak.py
@@ -2,24 +2,21 @@ import socket
 import threading
 
 HOST = ''
-PORT = 9999
+PORT = 2222
 
 def create_connection(conn, addr):
-    print('Connected by', addr)
+    print('Connected by', addr, ' active clients: ', threading.active_count()-1)
     while 1:
         data = conn.recv(1024)
         txt_data = data.decode("UTF-8").strip()
         print(txt_data)
         if not data:
             break
-        elif txt_data == 'close':
+        elif txt_data.lower() == 'close':
             conn.send('dosvidos \n'.encode())
             break
         else:
-            rev_data = ""
-            for i in range(len(txt_data)):
-                rev_data += txt_data[-1 - i]
-            conn.send((rev_data + '\n').encode())
+            conn.send(data)
     conn.close()
     print("Connection from ", addr, " was closed")
     return
@@ -29,7 +26,7 @@ s.bind((HOST, PORT))
 s.listen(1)
 
 
-while True:
+while threading.active_count() < 12:
     conn, addr = s.accept()
     th = threading.Thread(target=create_connection, args=(conn,addr))
     th.start()
